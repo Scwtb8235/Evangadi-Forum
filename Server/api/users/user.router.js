c8d84030e3e2c8bcf91f2e8c2213c6ae557cb71e
Scwtb8235 <124462310+Scwtb8235@users.noreply.github.com>
2023-05-02 14:04:48 -0400

    daily updates

--- Server/api/users/user.router.js
+++ Server/api/users/user.router.js
@@ -1,4 +1,17 @@
-const router = require('express').Router();
-const {createUser} = require('./user.controller')
-router.post("/",createUser);
+const router = require("express").Router();
+
+//importing auth middleware
+const auth = require("../middleware/auth");
+
+const { createUser, getUserById, login } = require("./user.controller");
+
+//route new user to be registered using createUser controller
+router.post("/", createUser);
+
+//route existing user to be verified using auth middleware and getUserById
+router.get("/", auth, getUserById);
+
+//route existing user to be login using login controller
+router.post("/login", login);
+
 module.exports = router;
