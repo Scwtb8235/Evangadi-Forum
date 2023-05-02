f2144de065fc00f59425d6dcea78284e55ffa945
Scwtb8235 <124462310+Scwtb8235@users.noreply.github.com>
2023-05-01 22:11:35 -0400

    committing after some mionr changes

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require("express");
 const cors  = require("cors")
+const userRouter = require("./Server/api/users/user.router")
 const app = express();
 const port = process.env.PORT
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json())
+app.use("api/users",userRouter)
 app.get("/", function (req, res) {
     res.send("Hello World");
 });
