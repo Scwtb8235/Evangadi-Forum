c76ff9b9c5fad377ad125496dc25799bccd5c7d5
Scwtb8235 <124462310+Scwtb8235@users.noreply.github.com>
2023-05-03 14:26:22 -0400

    daily update

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ const port = process.env.PORT
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json())
-app.use("api/users",userRouter)
-app.get("/", function (req, res) {
-    res.send("Hello World");
-});
+app.use("/api/users", userRouter)
+
 
 app.listen(port, ()=>console.log(`listing at http://localhost:${port}`));
